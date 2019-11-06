467bbc36b772fa30b82ceba3235177b5c77a4471
maggiermaguire <47397669+maggiermaguire@users.noreply.github.com>
2019-11-06 13:31:12 -0500

    Add files via upload

---
+++ javascript/pageLinks.js
@@ -0,0 +1,70 @@
+
+$("#AMTri").click(function(){
+                    window.location.href="pages/aboutMe.html";
+            });
+$("#PortTri").click(function(){
+                    window.location.href="pages/myWork.html";
+            });
+
+$("#ResTri").click(function(){
+                    window.open("pages/ResumePortfolio.pdf");
+            });
+
+$("#geoLocation").click(function(){
+                    window.location.href="../pages/workPages/geoLocationPage.html";
+            });
+$("#colorWheel").click(function(){
+                    window.location.href="../pages/workPages/colorWheelPage.html";
+            });
+$("#clubHub").click(function(){
+                    window.location.href="../pages/workPages/clubHubApp.html";
+            });
+$("#roommateFinder").click(function(){
+                    window.location.href="../pages/workPages/livable.html";
+            });
+$("#marbleSolitaire").click(function(){
+                    window.location.href="../pages/workPages/marbleSolitaire.html";
+            });
+$("#foodComparison").click(function(){
+                    window.location.href="../pages/workPages/foodComparison.html";
+            });
+
+$("#newsGame").click(function(){
+                    window.location.href="../pages/workPages/newsGame.html";
+            });
+$("#homeLink").click(function(){
+                    window.location.href="../index.html";
+            });
+$("#homeLink2").click(function(){
+                    window.location.href="../../index.html";
+            });
+$("#portLink").click(function(){
+                    window.location.href="../myWork.html";
+            });
+$("#WF1Web").click(function(){
+                    window.open("https://xd.adobe.com/spec/fe37b39b-9caf-433c-472a-82148baad58c-8806/");
+            });
+$("#WF1Mobile").click(function(){
+                    window.open("https://xd.adobe.com/spec/6beac49b-5f26-4d8e-62bc-66e34f6086c1-1ddb/");
+            });
+$("#WF2Web").click(function(){
+                    window.open("https://xd.adobe.com/spec/6b203715-02d0-4fe5-4271-fc54e9643eab-1d11/");
+            });
+$("#WF2Mobile").click(function(){
+                    window.open("https://xd.adobe.com/spec/ff3db594-516f-4ec3-79e2-2a250d4f1c16-890c/");
+            });
+$("#clubHubLink").click(function(){
+                    window.open("https://xd.adobe.com/spec/75c0f28d-1368-4939-6ac5-ce20bd5af413-03f1/screen/f0ae48f0-6952-45b0-bfa9-d5cf83eeeab7/1");
+            });
+$("#ConsWeb").click(function(){
+                    window.open("https://xd.adobe.com/spec/4324cbaf-73ae-4cd8-737b-6278117d4ce7-3515/");
+            });
+$("#ConsMobile").click(function(){
+                    window.open("https://xd.adobe.com/spec/ec13dac1-ef86-45fe-5953-32aafbdc0bb2-9a22/");
+            });
+$("#ConsTablet").click(function(){
+                    window.open("https://xd.adobe.com/spec/1f1e92d9-f463-46a4-4352-6206914bd594-692f/");
+            });
+$("#ConsCode").click(function(){
+                    window.open("https://maggiermaguire.github.io/midtermProject/finalProductindex.html");
+            });
